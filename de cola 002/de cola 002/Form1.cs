d4d5d215265c9f4df9b728923a6bcf24e89bdb30
armandonum <114707950+armandonum@users.noreply.github.com>
2022-11-22 18:58:31 -0400

    Add files via upload
    nanada de cola

---
+++ de cola 002/de cola 002/Form1.cs
@@ -0,0 +1,82 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace de_cola_002
+{
+    public partial class Form1 : Form
+    {
+        public decola c = new decola();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string x = textBox1.Text;
+            if (c.Llena()) MessageBox.Show("la cola ya esta llena ");
+            else
+            {
+                c.Insertar(x);
+                listBox1.Items.Add(x);
+
+            }
+            textBox1.Clear();
+            textBox1.Focus();
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string x = textBox1.Text;
+            if (c.Llena()) MessageBox.Show("la pila esta llena ");
+            else
+            {
+                c.Insertar(x);
+                listBox1.Items.Insert(0,x);
+
+            }
+
+            textBox1.Clear();
+            textBox1.Focus();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            string x;
+            if (c.Vacia()) MessageBox.Show("la cola ya esta vacia ");
+            else
+            {
+               x= c.Eliminar();
+                listBox1.Items.Remove(x);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            
+            string x;
+            if (c.Vacia()) MessageBox.Show("la cola esta vacia ");
+            else {
+                x = c.Eliminar();
+                listBox1.Items.RemoveAt(c.ultimo);
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ de cola 002/de cola 002/decola.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace de_cola_002
+{
+    public  class decola
+    {
+        public string[] coladatos = new string[100];
+        public int primero = 0;
+        public int ultimo = -1;
+        public int MaxCola = 4;
+        public string valor = "";
+        public decola()
+        {
+            primero = 0;
+            ultimo = -1;
+        }
+
+        public bool Vacia()
+        {
+            if (ultimo < primero) return true;
+            else return false;
+        }
+
+        public bool Llena()
+        {
+            if (ultimo == MaxCola - 1) return true;
+            else return false;
+        }
+
+        public void Insertar(string insert)
+        {
+            if (Llena()) MessageBox.Show("Error: La cola de elementos esta LLENA");
+            else
+            {
+                ultimo++;
+                coladatos[ultimo] = insert;
+            }
+        }
+
+        public string Eliminar()
+        {
+            string valor = " ";
+            if (Vacia()) MessageBox.Show("Error: La cola de elementos esta VACIA");
+            else
+            {
+                valor = coladatos[primero];
+                for (int i = 0; i < ultimo; i++)
+                {
+                    coladatos[i] = coladatos[i + 1];
+                }
+                ultimo--;
+            }
+            return valor;
+        }
+    }
+}
+    
+
